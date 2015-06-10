4ccd63ce35caccb3e29899c71979031f807f359e
Fabien Bavent <fabien.bavent@gmail.com>
2015-06-10 15:03:55 +0200

    [!] Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+console.log('Initializing...')
+
+var axnet = require('axnet'),
+    markup = require('html-markup')
+    Server = axnet.WebServer, 
+    app = new Server()
+
+
+
+
+markup.directory = __dirname + '/views/'
+markup.debug = false // Enable/Disable extra logs
+
+var auth = { 
+  auth: 'Basic realm="axFab.net"',
+  users:[
+    { name:'admin', password:'alpine' }
+  ]
+}
+
+// Public static directories
+app.use('/favicon.ico', Server.static(__dirname + '/dist'))
+app.use('/dist', Server.static(__dirname + '/dist'))
+app.use('/node_modules', Server.static(__dirname + '/node_modules'))
+app.use('/src', Server.static(__dirname + '/src'))
+
+app.get('/data', Server.static(__dirname + '/data'))
+app.post('/data', Server.basicAuth(auth))
+app.post('/data', Server.store(__dirname + '/data'))
+
+
+
+app.post('/app', Server.service(axnet.MailService))
+
+// *.html pages (also handle directories)
+app.use('/admin', Server.basicAuth(auth))
+app.use('/', markup.renderer(__dirname + '/views/'))
+
+var port = process.env.PORT || 80
+app.listen(port, function () {
+  console.log('Server listening on port ' + port)
+})
+
+
+// TODO Add Job
+
+var jobs = [
+  'cd bootstrap-3.3.1 && grunt watch'
+]
+
+//  openssl genrsa -out axfab.net.key 2048
+//  openssl req -new -x509 -key axfab.net.key -out axfab.net.cert -days 3650 -subj /CN=axfab.net
